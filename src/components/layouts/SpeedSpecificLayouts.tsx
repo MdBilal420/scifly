49e5d9472f95a26b80f12e5c9add0c080a526ae0
Mohammad Bilal <mohdbilal16@gmail.com>
2025-08-07 17:43:05 +0530

    refactor: Remove unused useState import from SpeedSpecificLayouts

--- src/components/layouts/SpeedSpecificLayouts.tsx
+++ src/components/layouts/SpeedSpecificLayouts.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { MdArrowBack, MdArrowForward } from 'react-icons/md'
 
